3cb20c38d124ed3313c0c186ccec62e325c6d862
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-15 19:29:03 +0000

    MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
+const colors = require("colors");
 
 mongoose.set("strictQuery", false);
 
 const connectDB = async (url) => {
   try {
     const conn = await mongoose.connect(url);
-    console.log(`\nMongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    console.log(
+      `\nMongoDB Connected: ${conn.connection.host}\n`.cyan.underline
+    );
   } catch (error) {
     console.log(error);
     process.exit(1);
